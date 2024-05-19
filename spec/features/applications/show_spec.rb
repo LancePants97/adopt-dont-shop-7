1f4823dda51605036b8f4a3a441a5407e6fa328a
Lance Nelson <lancen04@gmail.com>
2024-05-19 15:08:28 -0400

    removed a line

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -227,6 +227,32 @@ RSpec.describe Application do
 		click_on("Adopt this Pet")
 		expect(page).to have_current_path("/applications/#{@app2.id}")
 		expect(page).to have_content("Buddy")
-
 	end
+
+	# As a visitor
+# When I visit an application's show page
+# And I have added one or more pets to the application
+# Then I see a section to submit my application
+# And in that section I see an input to enter why I would make a good owner for these pet(s)
+# When I fill in that input
+# And I click a button to submit this application
+# Then I am taken back to the application's show page
+# And I see an indicator that the application is "Pending"
+# And I see all the pets that I want to adopt
+# And I do not see a section to add more pets to this application
+
+xit 'lets you submit the application' do
+	visit "/applications/#{@app1.id}"
+	
+	expect(page).to have_content("Submit application")
+	expect(page).to have_content("Why I would make a good owner for these pet(s)")
+	# note that good owner and good home are two separate fields
+	fill_in :why_i_would_make_a_good_owner, with: "I had a dog growing up"
+	click_on("Submit application")
+	expect(page).to have_current_path "/applications/#{@app1.id}"
+	expect(page).to have_content("Pending")
+	expect(page).to have_content("Mittens")
+	expect(page).to have_content("Buddy")
+	expect(page).to_not have_content("Adopt this Pet")
+end
 end
